505f9460aa4105be49707641daa578eeacfba87c
conor-darcy <conor-darcy@users.noreply.github.com>
2025-06-26 09:53:20 +0000

    deploy: 2ef3720fa932d7a1ee48d521fe1fce33f5aba6f0

---
+++ assets/my-component.stories-UXORRZzF.js
@@ -0,0 +1,6 @@
+const s={title:"Components/MyComponent",component:"my-component",parameters:{layout:"centered"},argTypes:{first:{control:"text",description:"The first name"},last:{control:"text",description:"The last name"}},tags:["autodocs"]},t={args:{first:"Stencil",last:"Storybook"}};var e,o,r;t.parameters={...t.parameters,docs:{...(e=t.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  args: {
+    first: "Stencil",
+    last: "Storybook"
+  }
+}`,...(r=(o=t.parameters)==null?void 0:o.docs)==null?void 0:r.source}}};const a=["Default"];export{t as Default,a as __namedExportsOrder,s as default};
